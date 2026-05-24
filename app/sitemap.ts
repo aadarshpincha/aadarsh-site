2b312d7af765a92775dee82c64e7f716ffb13492
Aadarsh Pincha <aadarsh.pincha@gmail.com>
2026-05-24 10:57:21 +0530

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://aadarsh-site.vercel.app',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://aadarsh-site.vercel.app/about',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://aadarsh-site.vercel.app/research',
+        lastModified: new Date(),
+      },
+    ]
+  }
